b125784d03b6dc1624054d453cd7030ad8e8804e
ubing <ubing0101@kakao.com>
2025-07-01 17:43:43 +0900

    분양게시판 init

---
+++ animalboard-service/src/main/java/com/playdata/animalboardservice/AnimalboardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.playdata.animalboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalboardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalboardServiceApplication.class, args);
+    }
+
+}
