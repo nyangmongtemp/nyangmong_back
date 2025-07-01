00c4794776980d457aa459acb477d178c2264eca
ubing <ubing0101@kakao.com>
2025-07-01 15:36:20 +0900

    크롤링 기능 추가

---
+++ scheduler-service/src/main/java/com/playdata/schedulerservice/crawling/entity/PetEvent.java
@@ -0,0 +1,55 @@
+package com.playdata.schedulerservice.crawling.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Getter @ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "pet_event")
+public class PetEvent {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long festivalId;
+    private String source;
+    private String title;
+    private String url;
+    private String location;
+    private String festivalDate;
+    private String festivalTime;
+    private String money;
+    private String imagePath;
+    private String reservationDate;
+    @Column(nullable = false, unique = true)
+    private String hash;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    public void updateFrom(PetEvent sourceEvent) {
+        this.source = sourceEvent.getSource();
+        this.title = sourceEvent.getTitle();
+        this.url = sourceEvent.getUrl();
+        this.location = sourceEvent.getLocation();
+        this.festivalDate = sourceEvent.getFestivalDate();
+        this.festivalTime = sourceEvent.getFestivalTime();
+        this.money = sourceEvent.getMoney();
+        this.imagePath = sourceEvent.getImagePath();
+        this.reservationDate = sourceEvent.getReservationDate();
+    }
+}
