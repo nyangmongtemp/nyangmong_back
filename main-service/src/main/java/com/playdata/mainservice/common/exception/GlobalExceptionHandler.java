e413fa3a7005912a7f92fc72e68b12f69644b369
EunHyeok <secun77@naver.com>
2025-07-07 12:01:33 +0900

    user-service, main-service 1. import문 오류 수정

--- main-service/src/main/java/com/playdata/mainservice/common/exception/GlobalExceptionHandler.java
+++ main-service/src/main/java/com/playdata/mainservice/common/exception/GlobalExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.playdata.mainservice.common.exception;
 
-import com.playdata.animalboardservice.common.dto.ErrorResponse;
-import com.playdata.animalboardservice.common.enumeration.ErrorCode;
+import com.playdata.mainservice.common.dto.ErrorResponse;
+import com.playdata.mainservice.common.enumeration.ErrorCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
