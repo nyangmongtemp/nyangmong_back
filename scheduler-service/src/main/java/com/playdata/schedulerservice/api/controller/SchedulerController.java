a672cd2796081f06bc9c6f56543728eeede9faba
ubing <ubing0101@kakao.com>
2025-07-18 12:53:41 +0900

    csv -> db 동물병원 기록 정보 저장

--- scheduler-service/src/main/java/com/playdata/schedulerservice/api/controller/SchedulerController.java
+++ scheduler-service/src/main/java/com/playdata/schedulerservice/api/controller/SchedulerController.java
@@ -1,12 +1,14 @@
 package com.playdata.schedulerservice.api.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,11 +21,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping
+@Slf4j
 public class SchedulerController {
 
     private final JobLauncher jobLauncher;
     private final Job syncAnimalJob;
     private final Job syncMapJob;
+
+    private final Job csvToDbJob;
 
 
     @GetMapping("/scheduler/api/animal")
@@ -53,6 +58,29 @@ public class SchedulerController {
 
         } catch (Exception e) {
             return "배치 실행 실패: " + e.getMessage();
+        }
+    }
+
+    @PostMapping("/scheduler/csv-to-db")
+    public String runCsvToDbJob() {
+        try {
+            // Spring Batch는 같은 파라미터로는 한 번만 실행되는 규칙이 있음.
+            // 매번 다른 파라미터를 만들면 같은 배치를 여러 번 실행할 수 있습니다.
+            JobParameters jobParameters = new JobParametersBuilder()
+                    .addLong("timestamp", System.currentTimeMillis()) // 현재 시간 추가
+                    .toJobParameters();
+
+            log.info(" ========== CSV To Database 배치 작업 시작! =========");
+            JobExecution jobExecution = jobLauncher.run(csvToDbJob, jobParameters);
+            log.info(" ========== 배치 완료! 상태: {} =========", jobExecution.getStatus());
+
+            return String.format("배치 실행 완료! 상태: %s, 처리된 아이템 수: %d",
+                    jobExecution.getStatus(),
+                    jobExecution.getStepExecutions().iterator().next().getWriteCount());
+
+        } catch (Exception e) {
+            log.error("배치 실행 중 오류 발생!", e);
+            return "배치 실행 실패!: " + e.getMessage();
         }
     }
 }
