4e360204db2eda4c4c48cfb88e575b8778748f99
EunHyeok <secun77@naver.com>
2025-07-20 17:51:30 +0900

    map-service 동물병원 데이터 중 EPSG:5174를 WGS84(표준 위경도좌표)로 변환 중이나 값의 오차 문제 직면

--- map-service/src/main/java/com/playdata/mapservice/map/service/Epsg5174ToWgs84Converter.java
+++ map-service/src/main/java/com/playdata/mapservice/map/service/Epsg5174ToWgs84Converter.java
@@ -7,18 +7,32 @@ public class Epsg5174ToWgs84Converter {
     // EPSG5174 좌표를 표준 위도, 경도로 변환해주는 메소드
     public static ProjCoordinate convertEPSG5174ToWGS84(double x, double y) {
         CRSFactory crsFactory = new CRSFactory();
-        CoordinateReferenceSystem epsg5174 = crsFactory.createFromName("EPSG:5174");
+
+        // EPSG:5174 정의 (TM 중부 좌표계)
+        String epsg5174Params = "+proj=tmerc +lat_0=38 +lon_0=127 +k=1.0 "
+                + "+x_0=200000 +y_0=500000 +ellps=GRS80 +units=m +no_defs";
+        CoordinateReferenceSystem epsg5174 = crsFactory.createFromParameters("EPSG:5174", epsg5174Params);
+
+        // EPSG:3857 (Web Mercator)
+        CoordinateReferenceSystem epsg3857 = crsFactory.createFromName("EPSG:3857");
+
+        // EPSG:4326 (WGS84, 위도/경도)
         CoordinateReferenceSystem wgs84 = crsFactory.createFromName("EPSG:4326");
 
         CoordinateTransformFactory ctFactory = new CoordinateTransformFactory();
-        CoordinateTransform transform = ctFactory.createTransform(epsg5174, wgs84);
 
-        ProjCoordinate srcCoord = new ProjCoordinate(x, y);
-        ProjCoordinate destCoord = new ProjCoordinate();
+        // 1단계: EPSG:5174 → EPSG:3857
+        CoordinateTransform to3857 = ctFactory.createTransform(epsg5174, epsg3857);
+        ProjCoordinate input = new ProjCoordinate(x, y);
+        ProjCoordinate coord3857 = new ProjCoordinate();
+        to3857.transform(input, coord3857);
 
-        transform.transform(srcCoord, destCoord);
+        // 2단계: EPSG:3857 → WGS84
+        CoordinateTransform toWGS84 = ctFactory.createTransform(epsg3857, wgs84);
+        ProjCoordinate finalCoord = new ProjCoordinate();
+        toWGS84.transform(coord3857, finalCoord);
 
-        return destCoord;
+        return finalCoord; // .x = longitude, .y = latitude
     }
 
 }
