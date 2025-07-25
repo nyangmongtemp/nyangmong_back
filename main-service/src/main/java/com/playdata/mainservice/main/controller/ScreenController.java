80336888db974233e1e771758e71bf503288d7e6
KangWonBeen <WonKong01@naver.com>
2025-07-25 14:40:54 +0900

    임시 커밋

---
+++ main-service/src/main/java/com/playdata/mainservice/main/controller/ScreenController.java
@@ -0,0 +1,14 @@
+package com.playdata.mainservice.main.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/screen")
+@RequiredArgsConstructor
+public class ScreenController {
+
+
+
+}
