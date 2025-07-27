c0e86a702aa109451fba7e4b1abc5f2373f139df
EunHyeok <secun77@naver.com>
2025-07-27 17:07:02 +0900

    map-service 1. 세종시 문제해결 (매우 좋지 않은 방식)

--- map-service/src/main/java/com/playdata/mapservice/map/service/CultureDetail/PetStyleSejongService.java
+++ map-service/src/main/java/com/playdata/mapservice/map/service/CultureDetail/PetStyleSejongService.java
@@ -3,25 +3,21 @@ package com.playdata.mapservice.map.service.CultureDetail;
 import com.playdata.mapservice.common.dto.CommonResDto;
 import com.playdata.mapservice.common.enumeration.ErrorCode;
 import com.playdata.mapservice.common.exception.CommonException;
-import com.playdata.mapservice.map.dto.CultureDetail.PetStyle.res.PetStyleDetailResDto;
 import com.playdata.mapservice.map.dto.CultureDetail.PetStyle.res.PetStyleListResDto;
-import com.playdata.mapservice.map.dto.CultureDetail.PetStyle.res.RegionDto;
 import com.playdata.mapservice.map.entity.CultureDetail.*;
 import com.playdata.mapservice.map.repository.cultureDetail.*;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class PetStyleService {
+public class PetStyleSejongService {
 
     private final PetStyleRepository petStyleRepository;
     private final PetCafeRepository petCafeRepository;
@@ -44,25 +40,25 @@ public class PetStyleService {
         List<String> detailRegion;
 
         if(category.equals("style")) {
-            detailRegion = petStyleRepository.findDetailRegion(targetRegion);
+            detailRegion = petStyleRepository.findDetailSejong(targetRegion);
         }
         else if (category.equals("cafe")) {
-            detailRegion = petCafeRepository.findDetailRegion(targetRegion);
+            detailRegion = petCafeRepository.findDetailSejong(targetRegion);
         }
         else if (category.equals("shop")) {
-            detailRegion = petShopRepository.findDetailRegion(targetRegion);
+            detailRegion = petShopRepository.findDetailSejong(targetRegion);
         }
         else if (category.equals("museum")) {
-            detailRegion = museumRepository.findDetailRegion(targetRegion);
+            detailRegion = museumRepository.findDetailSejong(targetRegion);
         }
         else if (category.equals("art")) {
-            detailRegion = artRepository.findDetailRegion(targetRegion);
+            detailRegion = artRepository.findDetailSejong(targetRegion);
         }
         else if (category.equals("literary")) {
-            detailRegion = literaryCenterRepository.findDetailRegion(targetRegion);
+            detailRegion = literaryCenterRepository.findDetailSejong(targetRegion);
         }
         else if(category.equals("drug")){
-            detailRegion = drugStoreRepository.findDetailRegion(targetRegion);
+            detailRegion = drugStoreRepository.findDetailSejong(targetRegion);
         } else {
             throw new CommonException(ErrorCode.BAD_REQUEST, "요청 중 오류 발생");
         }
@@ -78,74 +74,31 @@ public class PetStyleService {
 
         List<PetStyleListResDto> result;
         if(category.equals("style")) {
-            result = petStyleRepository.findListByRegion(CultureAddressCode.from(addressCode).getDesc(), sigungu)
+            result = petStyleRepository.findListByRegionSejong(CultureAddressCode.from(addressCode).getDesc(), sigungu)
                     .stream().map(PetStyle::toPetStyleListResDto).collect(Collectors.toList());
         } else if(category.equals("cafe")) {
-            result = petCafeRepository.findListByRegion(CultureAddressCode.from(addressCode).getDesc(), sigungu)
+            result = petCafeRepository.findListByRegionSejong(CultureAddressCode.from(addressCode).getDesc(), sigungu)
                     .stream().map(PetCafe::toPetStyleListResDto).collect(Collectors.toList());
         } else if(category.equals("shop")) {
-            result = petShopRepository.findListByRegion(CultureAddressCode.from(addressCode).getDesc(), sigungu)
+            result = petShopRepository.findListByRegionSejong(CultureAddressCode.from(addressCode).getDesc(), sigungu)
                     .stream().map(PetShop::toPetStyleListResDto).collect(Collectors.toList());
         } else if (category.equals("drug")) {
-            result = drugStoreRepository.findListByRegion(CultureAddressCode.from(addressCode).getDesc(), sigungu)
+            result = drugStoreRepository.findListByRegionSejong(CultureAddressCode.from(addressCode).getDesc(), sigungu)
                     .stream().map(DrugStore::toPetStyleListResDto).collect(Collectors.toList());
         } else if (category.equals("art")) {
-            result = artRepository.findListByRegion(CultureAddressCode.from(addressCode).getDesc(), sigungu)
+            result = artRepository.findListByRegionSejong(CultureAddressCode.from(addressCode).getDesc(), sigungu)
                     .stream().map(Art::toPetStyleListResDto).collect(Collectors.toList());
         } else if (category.equals("literary")) {
-            result = literaryCenterRepository.findListByRegion(CultureAddressCode.from(addressCode).getDesc(), sigungu)
+            result = literaryCenterRepository.findListByRegionSejong(CultureAddressCode.from(addressCode).getDesc(), sigungu)
                     .stream().map(LiteraryCenter::toPetStyleListResDto).collect(Collectors.toList());
         } else if (category.equals("museum")) {
-            result = museumRepository.findListByRegion(CultureAddressCode.from(addressCode).getDesc(), sigungu)
+            result = museumRepository.findListByRegionSejong(CultureAddressCode.from(addressCode).getDesc(), sigungu)
                     .stream().map(Museum::toPetStyleListResDto).collect(Collectors.toList());
         } else {
             throw new CommonException(ErrorCode.BAD_REQUEST, "요청 중 오류 발생");
         }
 
         return new CommonResDto(HttpStatus.OK, "해당 지역의 미용실 정보 찾음", result);
-    }
-
-    public CommonResDto findDetail(Long id, String category) {
-
-        if(!categoryList.contains(category)) {
-            throw new CommonException(ErrorCode.BAD_REQUEST, "잘못된 카테고리 입력값입니다.");
-        }
-
-        PetStyleDetailResDto found;
-
-        if(category.equals("style")) {
-            found = petStyleRepository.findById(id)
-                    .orElseThrow(() -> new CommonException(ErrorCode.BAD_REQUEST))
-                    .toPetStyleDetailResDto();
-        } else if (category.equals("cafe")) {
-            found = petCafeRepository.findById(id)
-                    .orElseThrow(() -> new CommonException(ErrorCode.BAD_REQUEST))
-                    .toPetStyleDetailResDto();
-        } else if (category.equals("drug")) {
-            found = drugStoreRepository.findById(id)
-                    .orElseThrow(() -> new CommonException(ErrorCode.BAD_REQUEST))
-                    .toPetStyleDetailResDto();
-        } else if (category.equals("art")) {
-            found = artRepository.findById(id)
-                    .orElseThrow(() -> new CommonException(ErrorCode.BAD_REQUEST))
-                    .toPetStyleDetailResDto();
-        } else if (category.equals("museum")) {
-            found = museumRepository.findById(id)
-                    .orElseThrow(() -> new CommonException(ErrorCode.BAD_REQUEST))
-                    .toPetStyleDetailResDto();
-        } else if (category.equals("shop")) {
-            found = petShopRepository.findById(id)
-                    .orElseThrow(() -> new CommonException(ErrorCode.BAD_REQUEST))
-                    .toPetStyleDetailResDto();
-        } else if (category.equals("literary")) {
-            found = literaryCenterRepository.findById(id)
-                    .orElseThrow(() -> new CommonException(ErrorCode.BAD_REQUEST))
-                    .toPetStyleDetailResDto();
-        } else {
-            throw new CommonException(ErrorCode.BAD_REQUEST, "요청 중 오류 발생");
-        }
-
-        return new CommonResDto(HttpStatus.OK, "해당 미용실의 상세 정보 찾음", found);
     }
 
 }
