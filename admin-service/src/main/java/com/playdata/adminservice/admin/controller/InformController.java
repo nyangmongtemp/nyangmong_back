f3f33c2712a80b160353814456205333c99599ad
ubing <ubing0101@kakao.com>
2025-08-01 12:51:13 +0900

    [관리자] 문의 스웨거

--- admin-service/src/main/java/com/playdata/adminservice/admin/controller/InformController.java
+++ admin-service/src/main/java/com/playdata/adminservice/admin/controller/InformController.java
@@ -29,7 +29,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequiredArgsConstructor
 @PreAuthorize("hasAnyRole('BOSS', 'CUSTOMER')")
-public class InformController {
+public class InformController implements InformControllerDocs{
 
     private final InformService informService;
 
