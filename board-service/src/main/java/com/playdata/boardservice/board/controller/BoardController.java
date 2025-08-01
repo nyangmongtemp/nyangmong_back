8a7a9632e98321043ea986c2ffd7afcb80077ef3
EunHyeok <secun77@naver.com>
2025-08-01 17:18:45 +0900

    board-service 1. implements 누락 수정

--- board-service/src/main/java/com/playdata/boardservice/board/controller/BoardController.java
+++ board-service/src/main/java/com/playdata/boardservice/board/controller/BoardController.java
@@ -34,7 +34,7 @@ import java.util.List;
 @RequestMapping("/board")
 @RequiredArgsConstructor
 @Slf4j
-public class BoardController {
+public class BoardController implements BoardControllerDocs {
 
     private final BoardService boardService;
     private final JwtTokenProvider jwtTokenProvider;
