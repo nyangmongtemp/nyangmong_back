371d728cf344f36c9919d367c73e4d5f69cb84fc
EunHyeok <secun77@naver.com>
2025-08-05 17:14:47 +0900

    devOps - k8s 시작

--- user-service/src/main/java/com/playdata/userservice/user/dto/req/UserSaveReqDto.java
+++ user-service/src/main/java/com/playdata/userservice/user/dto/req/UserSaveReqDto.java
@@ -6,7 +6,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.time.LocalDateTime;
 
