678a2d7b45ef43df7a527bdd56eba8104dbfdf48
EunHyeok <secun77@naver.com>
2025-08-07 14:07:40 +0900

    devOps - cors 잡는중

---
+++ gateway-service/src/main/java/com/playdata/gatewayservice/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.playdata.gatewayservice;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of("https://nyangmong.com"));
+        config.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);  // 모든 경로에 대해 설정 적용
+
+        return new CorsWebFilter(source);
+    }
+
+}
