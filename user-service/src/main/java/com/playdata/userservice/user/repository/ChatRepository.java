810866cdec74b3d87fb343a23228b862cb83ff6c
EunHyeok <secun77@naver.com>
2026-03-18 14:56:14 +0900

    수정본

--- user-service/src/main/java/com/playdata/userservice/user/repository/ChatRepository.java
+++ user-service/src/main/java/com/playdata/userservice/user/repository/ChatRepository.java
@@ -10,8 +10,8 @@ import java.util.Optional;
 public interface ChatRepository extends JpaRepository<Chat, Long> {
 
     // 사용자 간의 기존에 생성된 채팅방 조회
-    @Query("SELECT c FROM Chat c WHERE (c.userId1 = :userId1 AND c.userId2 = :userId2) OR " +
-            "(c.userId1 = :userId2 AND c.userId2 = :userId1) AND c.active = true")
+    @Query("SELECT c FROM Chat c WHERE ((c.userId1 = :userId1 AND c.userId2 = :userId2) OR " +
+            "(c.userId1 = :userId2 AND c.userId2 = :userId1)) AND c.active = true")
     Optional<Chat> findByUserId(Long userId1, Long userId2);
 
     // 사용자의 활성화된 모든 채팅방 조회
